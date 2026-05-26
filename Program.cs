26a8293152d00c7acf871d275329c9499f442b8c
b3agz <beagrie@gmail.com>
2026-05-26 16:06:30 +0100

    Refactor: Transition architecture from MonoGame to Raylib-cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 480, "Corely Engine Test");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.DarkBlue);
+    Raylib.DrawText("Raylib is connected.", 12, 12, 20, Color.RayWhite);
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
